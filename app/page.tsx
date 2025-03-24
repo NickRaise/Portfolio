246fc9c13ed2976841b5c515319c9337bc13af74
NickRaise <nikhilnainwaya17@gmail.com>
2025-03-24 11:42:11 +0530

    nextjs setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 className="bg-red-500">Hello Portfolio</h1>
+      </div>
+    </main>
+  );
+}
