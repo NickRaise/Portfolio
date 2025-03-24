56ae2bb8d7c429fa5dd990ab71037509b08c998b
NickRaise <nikhilnainwaya17@gmail.com>
2025-03-24 20:11:54 +0530

    added the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1 className="bg-red-500">Hello Portfolio</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+      <div className="max-w-7xl w-full">
+        <h1 className="bg-black-100">
+          <Hero />
+        </h1>
       </div>
     </main>
   );
