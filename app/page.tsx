0992b06b79024aa1a28fcf393a004c4c461089d8
NickRaise <nikhilnainwaya17@gmail.com>
2025-03-25 21:01:28 +0530

    added the rid menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
-        <h1 className="bg-black-100">
-          <Hero />
-        </h1>
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
       </div>
     </main>
   );
